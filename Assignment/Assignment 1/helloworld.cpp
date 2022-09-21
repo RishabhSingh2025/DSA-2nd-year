5dde571a97bb83071c66a9b576d9586fc72464de
RishabhSingh2025 <107607759+RishabhSingh2025@users.noreply.github.com>
2022-09-21 23:24:09 +0530

    Create helloworld.cpp

---
+++ Assignment/Assignment 1/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+
+https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
