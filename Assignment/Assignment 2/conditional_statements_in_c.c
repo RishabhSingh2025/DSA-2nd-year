b79b7073888246dbcefa91608d2cd693b4da631a
RishabhSingh2025 <107607759+RishabhSingh2025@users.noreply.github.com>
2022-09-25 23:16:53 +0530

    Create conditional_statements_in_c.c

---
+++ Assignment/Assignment 2/conditional_statements_in_c.c
@@ -0,0 +1,61 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==1)
+    {
+        printf("one \n");
+    }
+    else if(a==2)
+    {
+        printf("two\n");
+    }
+        else if(a==3)
+    {
+        printf("three \n");
+    }
+    else if(a==4)
+    {
+        printf("four \n");
+    }
+    else if(a==5)
+    {
+        printf("five \n");
+    }
+    else if(a==6)
+    {
+        printf("six \n");
+    }
+    else if(a==7)
+    {
+        printf("seven \n");
+    }
+    else if(a==8)
+    {
+        printf("eight \n");
+    }
+    else if(a==9)
+    {
+        printf("nine \n");
+    }
+    else if(a>9)
+    {
+        printf("Greater than 9 \n");
+    }
+    return 0;
+}
+
+
+https://www.hackerrank.com/challenges/conditional-statements-in-c/submissions/code/292525190
