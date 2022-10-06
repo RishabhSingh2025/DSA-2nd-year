571dac17d6cd1dc8986cf88fbb776b3ee2353ae3
RishabhSingh2025 <107607759+RishabhSingh2025@users.noreply.github.com>
2022-10-06 23:24:58 +0530

    Create solve_me_first.cpp

---
+++ Assignment/Assignment 3/solve_me_first.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  int sum=a+b;
+  return sum;
+}
+
+int main() {
+  int a,b;
+  int sum;
+  cin>>a>>b;
+  sum=solveMeFirst(a,b);
+  cout<<sum;
+  return 0;
+}
+
+https://www.hackerrank.com/challenges/solve-me-first/submissions/code/292975445
