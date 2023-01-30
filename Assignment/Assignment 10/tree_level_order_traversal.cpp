3a97e8db40dc5110f762fd1d6cbeb590a304f20c
RishabhSingh2025 <107607759+RishabhSingh2025@users.noreply.github.com>
2023-01-30 22:03:56 +0530

    Create tree_level_order_traversal.cpp

---
+++ Assignment/Assignment 10/tree_level_order_traversal.cpp
@@ -0,0 +1,30 @@
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            Node* root = q.front();
+            q.pop();
+            cout << root->data << ' ';
+            if (root->left) q.push(root->left);
+            if (root->right) q.push(root->right);
+        }
+
+
+    }
+    
+    https://www.hackerrank.com/challenges/tree-level-order-traversal/submissions/code/310357537
