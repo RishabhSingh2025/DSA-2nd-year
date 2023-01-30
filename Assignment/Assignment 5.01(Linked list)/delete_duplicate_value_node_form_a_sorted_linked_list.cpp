19c136f67b7deba29266925e14c9213a189fd7c7
RishabhSingh2025 <107607759+RishabhSingh2025@users.noreply.github.com>
2023-01-30 22:24:20 +0530

    Create delete_duplicate_value_node_form_a_sorted_linked_list.cpp

---
+++ Assignment/Assignment 5.01(Linked list)/delete_duplicate_value_node_form_a_sorted_linked_list.cpp
@@ -0,0 +1,31 @@
+/*
+ * Complete the 'removeDuplicates' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* llist) {
+    SinglyLinkedListNode *curr = llist;
+    while (curr != NULL) {
+        SinglyLinkedListNode *next = curr->next;
+        while (next != NULL && next->data == curr->data)
+            next = next->next;
+        curr->next = next;
+        curr = next;
+        if (next != NULL) next = next->next;
+    }
+    return llist;
+}
+
+https://www.hackerrank.com/challenges/delete-duplicate-value-nodes-from-a-sorted-linked-list/submissions/code/311941736
