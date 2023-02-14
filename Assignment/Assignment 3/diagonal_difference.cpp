62dbd782e5cca3ed6ada0031c1931ec1de02c1e9
RishabhSingh2025 <107607759+RishabhSingh2025@users.noreply.github.com>
2023-02-14 18:33:26 +0530

    Update diagonal_difference.cpp

--- Assignment/Assignment 3/diagonal_difference.cpp
+++ Assignment/Assignment 3/diagonal_difference.cpp
@@ -1,29 +1,27 @@
-#include<iostream>
-using namespace std;
-int main()
+int diagonalDifference(vector<vector<int>> arr) 
 {
-    int n,sum1=0,sum2=0;
-    cin>>n;
-    int arr1[n][n];
+    int n=arr.size();
+    int sum1=0;
+    int sum2=0;
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=i;j<=i;j++)
         {
-            cin>>arr1[i][j];
+            sum1=sum1+arr[i][j];
         }
     }
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
         {
-            if(i==j)
-            sum1=sum1+arr1[i][j];
-            if(i+j==n-1)
-            sum2=sum2+arr1[i][j];
+            if((i+j)==(n-1))
+            {
+                sum2=sum2+arr[i][j];
+            }
         }
     }
-    cout<<abs(sum1-sum2);
-    return 0;
+    int difference=sum1-sum2;
+    return abs(difference);
 }
 
 
